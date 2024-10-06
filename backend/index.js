270e115995f494256d4d71811ff0431779cf2981
millenxs <milenameedeiros@gmail.com>
2024-10-06 00:14:13 -0300

    colors edited & cors origin

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { GoogleGenerativeAI } = require('@google/generative-ai'); 
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 
--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -6,6 +6,7 @@ const ChatBot = () => {
     { sender: 'bot', text: 'Olá! Eu sou o VeganBot 🌱. Como posso te ajudar hoje? 😊' }
   ]);
   const [input, setInput] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
 
   const chatEndRef = useRef(null);
 
@@ -29,6 +30,8 @@ const ChatBot = () => {
 
 	  setInput(''); 
 
+	  setIsTyping(true);
+
       try {
         const response = await axios.post('http://localhost:3000/api/message', { message: input });
         let botMessageText = response.data.reply;
@@ -37,20 +40,22 @@ const ChatBot = () => {
 		
 		const botMessage = { text: response.data.reply, sender: 'bot' };
         setMessages((prevMessages) => [...prevMessages, botMessage]);
+		setIsTyping(false);
       } catch (error) {
         console.error('Error communicating with the bot:', error);
+		setIsTyping(false);
       }
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="bg-gray-800/70 rounded-3xl shadow-2xl w-full max-w-md h-[600px] flex flex-col overflow-hidden ">
-        <div className="bg-gray-700/70 p-4 flex justify-between items-center ">
-          <div className="title text-white text-lg">Chatbot de Veganismo</div>
-          <h2 className="text-gray-400 text-sm">@VeganBot 🌱</h2> 
+      <div className="bg-white/10 rounded-3xl shadow-2xl w-full max-w-md h-[500px] flex flex-col overflow-hidden ">
+        <div className="bg-[#95C457] p-4 flex justify-between items-center ">
+          <div className="title text-black text-lg">Chatbot de Veganismo</div>
+          <h2 className="text-gray-700 text-sm">@VeganBot 🌱</h2> 
           <figure className="avatar">
-            <img src="/BOT.png" width="50" height="50" className="rounded-full" />
+            <img src="/favicon.png" width="50" height="50" className="rounded-full" />
           </figure>
         </div>
 
@@ -62,29 +67,38 @@ const ChatBot = () => {
               className={
                 msg.sender === 'user'
                   ? 'bg-gray-600 text-white self-end p-2 rounded-lg max-w-xs'
-                  : 'bg-[#7BCBD5] text-black self-start p-2 rounded-lg max-w-xs'
+                  : 'bg-[#3a5321] text-white self-start p-2 rounded-lg max-w-xs'
               }
             >
               {msg.text}
             </div>
           ))}
+
+		{isTyping && (
+            <div className="self-start bg-[#3a5321] text-white p-2 rounded-lg max-w-xs">
+              <div className="typing-indicator">
+                VeganBot está digitando<span className="dots"><span>.</span><span>.</span><span>.</span></span>
+              </div>
+            </div>
+        )}
+
 		  <div ref={chatEndRef} />
         </div>
 
        
-        <div className="bg-gray-700/70  p-2 flex">
+        <div className="bg-[#95C457]  p-2 flex">
           <input
 		  type="text"
             placeholder="Digite sua mensagem..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyPress={(e) => e.key === 'Enter' && sendMessage()} 
-            className="flex-grow p-2 bg-gray-600 text-white rounded-md outline-none resize-none mb-4"
+            className="flex-grow p-2 bg-transparent ring-0 outline-none border border-neutral-500 text-neutral-900 placeholder-gray-700 text-sm rounded-lg focus:ring-gray-700 placeholder-opacity-50 focus:border-gray-800 w-full p-2.5 checked:bg-gray-800 text-white rounded-md outline-none resize-none mb-4"
             rows="3"
           ></input>
           <button
             onClick={sendMessage}
-            className="sendMessage ml-2 bg-[#7BCBD5] hover:bg-[#4FB3C1] font-bold py-2 px-4 rounded-md mb-4"
+            className="sendMessage ml-2 bg-[#ABD373] hover:bg-[#e7f3d4] font-bold py-2 px-4 rounded-md mb-4"
           >
             Enviar
           </button>
